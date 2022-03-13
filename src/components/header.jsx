c4539ee8c1e498514b40c5fefa8f8adbc4fd55a5
Ricardo Hernández <rmoguel@Ricardos-MacBook-Air.local>
2022-03-13 16:33:05 -0700

    Style update

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -6,11 +6,10 @@ export const Header = (props) => {
           <div className='container'>
             <div className='row'>
               <div className='col-md-8 col-md-offset-2 intro-text'>
-                <h1>
-                  {props.data ? props.data.title : 'Loading'}
                   <span></span>
-                </h1>
-                <p>{props.data ? props.data.paragraph : 'Loading'}</p>
+                
+                <h1>Daniela & Ricardo's Baby Girl</h1>
+                <p className="intro-text-subtitle">Baby shower</p>
                 <a
                   href='https://wa.me/12066968716'
                   className='btn btn-custom btn-lg page-scroll'
