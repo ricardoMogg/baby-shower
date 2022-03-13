0ccc4723ea3593de4eda8a0f44b5c7766ebcd676
Ricardo Hernández <rmoguel@Ricardos-MacBook-Air.local>
2022-03-13 15:04:33 -0700

    slight css adjust

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -12,7 +12,7 @@ export const About = (props) => {
           <div className="col-xs-12 col-md-6">
             <h2>Ubicacion del jardin</h2>
             <a href="https://www.google.com.mx/maps/place/Jardin+Mosada/@19.328962,-99.1214313,17z/data=!3m1!4b1!4m5!3m4!1s0x85ce01da80bc215f:0xbffaeba55ba856b1!8m2!3d19.328962!4d-99.1192426">
-            <img src="img/ubicacion-pink.jpg" className="img-responsive" style={{"min-height":"520px"}} alt="" />{" "}
+            <img src="img/ubicacion-pink.jpg" className="img-responsive" alt="" />{" "}
             </a>
             
           </div>
